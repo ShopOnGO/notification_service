3d3820ce59d4923bd48e971ad0ec9b022fbc458a
exttreme0001 <fpm.LukashovES@bsu.by>
2025-04-15 08:56:00 +0300

    model

--- internal/model/model.go
+++ internal/model/model.go
@@ -9,10 +9,3 @@ type Notification struct {
 	UserID   uint32                 `json:"userID" bson:"userID"`
 	Payload  map[string]interface{} `json:"payload" bson:"payload"`
 }
-
-// type Notification struct {
-// 	Category string                 `json:"category"` // auth, cart, payment
-// 	Subtype  string                 `json:"subtype"`  // login, add_item, success
-// 	UserID   uint32                 `json:"userID"`
-// 	Payload  map[string]interface{} `json:"payload"`
-// }
